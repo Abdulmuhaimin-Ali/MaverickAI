340f2275c7cacf5f620824787c23611a6f2ca4dd
aaron1-z <adityakittu2773@gmail.com>
2025-01-12 20:24:33 +0530

    scraper

---
+++ backend/scrapers/yahooFinanceScraper.js
@@ -0,0 +1,11 @@
+const fetchYahooFinanceData = require('./yahooFinanceScraper');
+
+test('fetchYahooFinanceData should return data for a valid ticker', async () => {
+  const data = await fetchYahooFinanceData('AAPL');
+  
+  // Expect the data object to have these properties
+  expect(data).toHaveProperty('ticker', 'AAPL');
+  expect(data).toHaveProperty('price');
+  expect(data).toHaveProperty('change');
+  expect(data).toHaveProperty('changePercent');
+}, 10000); // Increase timeout to 10 seconds
